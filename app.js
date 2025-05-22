381c42f5b9a0f7d5169487f8ef8b16b43ba8e975
Debdip Mukherjee <debdip.mukherjee@capitalnumbers.com>
2025-05-23 04:09:28 +0530

    base64 image upload modification

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ global.__basedir = __dirname;
 const db = require("./models/index.js");
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 const authRoutes = require("./routes/auth.routes");
