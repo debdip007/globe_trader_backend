a367c10a1ff3c4d56c5487ad496873937546f102
Debdip Mukherjee <debdip.mukherjee@capitalnumbers.com>
2025-05-22 23:43:58 +0530

    Get user ID from the Auth-Token

--- validators/product.validator.js
+++ validators/product.validator.js
@@ -7,9 +7,6 @@ const createProductValidator = [
   body('sku')
     .notEmpty()
     .withMessage('Product SKU is required.'),
-  body('main_image')
-    .notEmpty()
-    .withMessage('Product main image is required.'),
   body('category')
     .notEmpty()
     .withMessage('Category is required.')
