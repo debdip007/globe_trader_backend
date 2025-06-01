4b00e43983aa0dabfeeb2564fedc01b82444fb96
Debdip Mukherjee <debdip.mukherjee@capitalnumbers.com>
2025-06-02 01:48:34 +0530

    Additional Image, Profile Details, Interest API

--- migrations/20250528070559-modify-preference-table.js
+++ migrations/20250528070559-modify-preference-table.js
@@ -9,9 +9,14 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-    // await queryInterface.addColumn('UserPreferences', 'product_id', {
-    //   type: Sequelize.INTEGER,
-    // });
+    const tableDefinition = await queryInterface.describeTable("UserPreferences");
+
+    if (!tableDefinition["product_id"]) {
+      await queryInterface.addColumn("UserPreferences", "product_id", {
+        type: Sequelize.STRING, // Replace with your desired data type
+        allowNull: true,        // Set according to your requirements
+      });
+    }
   },
 
   async down (queryInterface, Sequelize) {
@@ -21,6 +26,6 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
-    // await queryInterface.removeColumn('UserPreferences', 'product_id');
+    await queryInterface.removeColumn('UserPreferences', 'product_id');
   }
 };
--- migrations/20250528190031-modify-user-profile-table.js
+++ migrations/20250528190031-modify-user-profile-table.js
@@ -9,39 +9,108 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    const tableDefinition = await queryInterface.describeTable("BuyerProfiles");
+
+    if (tableDefinition["operating_location"]) {
+      await queryInterface.removeColumn("BuyerProfiles", "operating_location", {
+        type: Sequelize.STRING, // Replace with your desired data type
+        allowNull: true,        // Set according to your requirements
+      });
+    }
+
+    if (!tableDefinition["business_address"]) {
+      await queryInterface.removeColumn("BuyerProfiles", "business_address", {
+        type: Sequelize.TEXT, // Replace with your desired data type
+        allowNull: true,        // Set according to your requirements
+      });
+    }
+
+    if (!tableDefinition["business_city"]) {
+      await queryInterface.removeColumn("BuyerProfiles", "business_city", {
+        type: Sequelize.TEXT, // Replace with your desired data type
+        allowNull: true,        // Set according to your requirements
+      });
+    }
+
+    if (!tableDefinition["business_state"]) {
+      await queryInterface.removeColumn("BuyerProfiles", "business_state", {
+        type: Sequelize.TEXT, // Replace with your desired data type
+        allowNull: true,        // Set according to your requirements
+      });
+    }
+
+    if (!tableDefinition["business_pincode"]) {
+      await queryInterface.removeColumn("BuyerProfiles", "business_pincode", {
+        type: Sequelize.TEXT, // Replace with your desired data type
+        allowNull: true,        // Set according to your requirements
+      });
+    }
+
+    const sellertableDefinition = await queryInterface.describeTable("SellerProfiles");
+
+    if (!sellertableDefinition["business_address"]) {
+      await queryInterface.removeColumn("SellerProfiles", "business_address", {
+        type: Sequelize.TEXT, // Replace with your desired data type
+        allowNull: true,        // Set according to your requirements
+      });
+    }
+
+    if (!sellertableDefinition["business_city"]) {
+      await queryInterface.removeColumn("SellerProfiles", "business_city", {
+        type: Sequelize.TEXT, // Replace with your desired data type
+        allowNull: true,        // Set according to your requirements
+      });
+    }
+
+    if (!sellertableDefinition["business_state"]) {
+      await queryInterface.removeColumn("SellerProfiles", "business_state", {
+        type: Sequelize.TEXT, // Replace with your desired data type
+        allowNull: true,        // Set according to your requirements
+      });
+    }
+
+    if (!sellertableDefinition["business_pincode"]) {
+      await queryInterface.removeColumn("SellerProfiles", "business_pincode", {
+        type: Sequelize.TEXT, // Replace with your desired data type
+        allowNull: true,        // Set according to your requirements
+      });
+    }
+
+    
+
     // await queryInterface.removeColumn('BuyerProfiles', 'operating_location');
 
-    await queryInterface.addColumn('BuyerProfiles', 'business_address', {
-      type: Sequelize.TEXT,
-    });
+    // await queryInterface.addColumn('BuyerProfiles', 'business_address', {
+    //   type: Sequelize.TEXT,
+    // });
 
-    await queryInterface.addColumn('BuyerProfiles', 'business_city', {
-      type: Sequelize.TEXT,
-    });
+    // await queryInterface.addColumn('BuyerProfiles', 'business_city', {
+    //   type: Sequelize.TEXT,
+    // });
 
-    await queryInterface.addColumn('BuyerProfiles', 'business_state', {
-      type: Sequelize.TEXT,
-    });
+    // await queryInterface.addColumn('BuyerProfiles', 'business_state', {
+    //   type: Sequelize.TEXT,
+    // });
 
-    await queryInterface.addColumn('BuyerProfiles', 'business_pincode', {
-      type: Sequelize.TEXT,
-    });
+    // await queryInterface.addColumn('BuyerProfiles', 'business_pincode', {
+    //   type: Sequelize.TEXT,
+    // });
 
-    await queryInterface.addColumn('SellerProfiles', 'business_address', {
-      type: Sequelize.TEXT,
-    });
+    // await queryInterface.addColumn('SellerProfiles', 'business_address', {
+    //   type: Sequelize.TEXT,
+    // });
 
-    await queryInterface.addColumn('SellerProfiles', 'business_city', {
-      type: Sequelize.TEXT,
-    });
+    // await queryInterface.addColumn('SellerProfiles', 'business_city', {
+    //   type: Sequelize.TEXT,
+    // });
 
-    await queryInterface.addColumn('SellerProfiles', 'business_state', {
-      type: Sequelize.TEXT,
-    });
+    // await queryInterface.addColumn('SellerProfiles', 'business_state', {
+    //   type: Sequelize.TEXT,
+    // });
 
-    await queryInterface.addColumn('SellerProfiles', 'business_pincode', {
-      type: Sequelize.TEXT,
-    });
+    // await queryInterface.addColumn('SellerProfiles', 'business_pincode', {
+    //   type: Sequelize.TEXT,
+    // });
   },
 
   async down (queryInterface, Sequelize) {
