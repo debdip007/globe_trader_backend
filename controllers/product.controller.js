1cd3d413806c15fe68f2b44b7907de2840e0891b
Debdip Mukherjee <debdip.mukherjee@capitalnumbers.com>
2025-07-13 17:12:53 +0530

    User response update

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -451,10 +451,7 @@ async function getUserDetails (userId, req = null) {
   try {    
     const user = await User.findByPk(userId);
     const obj = user.toJSON(); 
-    if(obj.profile_image != null) {
-      obj.profile_image = req.protocol  + '://' + req.get('host') + '/images/profile/' +obj.profile_image;
-    }
-    
+    obj.profile_image = req.protocol  + '://' + req.get('host') + '/images/profile/' +obj.profile_image;
     return obj;
   } catch (error) {
     console.error('Error getting user details:', error);
