58b4e546449175b861b703c47ded8dccea64f7dd
Debdip Mukherjee <debdip.mukherjee@capitalnumbers.com>
2025-10-23 01:35:52 +0530

    Backend API

--- routes/backend.routes.js
+++ routes/backend.routes.js
@@ -7,6 +7,7 @@ const { verifyToken } = require("../middlewares/auth.middleware");
 router.post('/user-list', verifyToken, backendController.getUserList);
 router.post('/product-list', verifyToken, backendController.getProductList);
 router.get('/role-list', verifyToken, backendController.getRoleList);
+router.get('/permission-list', verifyToken, backendController.getPermissionList);
 router.post('/modify-category', verifyToken, backendController.modifyCategory);
 
 module.exports = router;
